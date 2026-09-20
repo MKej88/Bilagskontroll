03fa57e5c7ace535c0444149e0f2830d3ea2046c
MKej88 <mads.kristensen2@gmail.com>
2026-09-20 10:32:54 +0200

    Filtrer bilagsutvalg på valgt år

---
+++ tests/test_make_sample.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+from gui import App
+
+
+class DummyVar:
+    def __init__(self, value):
+        self.value = value
+
+    def get(self):
+        return self.value
+
+
+class FakeApp:
+    def __init__(self):
+        self.df = pd.DataFrame(
+            {
+                "Fakturanr": ["2024-A", "2025-B", "2025-C"],
+                "Fakturadato": ["15.12.2024", "02.01.2025", "03.01.2025"],
+            }
+        )
+        self.sample_size_var = DummyVar("3")
+        self.year_var = DummyVar("2025")
+
+    def _ensure_helpers(self):
+        pass
+
+    def render(self):
+        pass
+
+
+def test_make_sample_bare_inneholder_valgt_aar():
+    app = FakeApp()
+
+    App.make_sample(app)
+
+    assert set(app.sample_df["Fakturadato"]) == {"02.01.2025", "03.01.2025"}
